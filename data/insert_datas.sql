6ed50466879e28d4d3b8775d245fc3bae53f372b
TouyardK <touyard.kevin@outlook.fr>
2021-10-23 17:03:41 +0200

    insert table projects

---
+++ data/insert_datas.sql
@@ -0,0 +1,10 @@
+#------------------------------------------------------------
+# Table: projects
+#------------------------------------------------------------
+
+INSERT INTO projects( title, content, icon) VALUES 
+('Région Sud', 'Notre client nous a confié la mission de créer un calculateur d impact environnemental, afin d assurer un suivis des évènements sportif régionaux.', 'regionsud.png'),
+('Hackathon-72h [Fiiver]', 'Nous avons développé un module complétementaire pour le site Fiiver, qui permet à la communautée de créer des évènements pour partager des moments de convivialité.', 'fiiver3.png'),
+('Oovy', 'Notre but était l apprentissage de l utilisation d une API, nous avons donc réalisé une apply dynamique sous React, qui permettait de rechercher des films avec des filtres.', 'logooovy.png'),
+('Portfolio AKGS', 'Projet en début de formation, en groupe de 4 nous avons réalisé un portfolio, en deux semaines qui representais la team et son parcours.', 'akgs.png');
+
