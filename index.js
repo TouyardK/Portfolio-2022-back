22ecbfcfd601b28cb5b3273065dffb41aab885e5
TouyardK <touyard.kevin@outlook.fr>
2021-10-24 11:17:37 +0200

    Display BDD with json and create routes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4040;
+const port = process.env.PORT || 8080;
 const homeRouter = express.Router();
 const projectRouter = express.Router();
+const connection = require('./db_config');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -22,11 +23,30 @@ homeRouter.get('/', getAllProjects, getAllProjectsSuite, (req, res) => {
 });
 
 projectRouter.get('/', (req, res) => {
-    res.send('My Projects')
+  const sql = `SELECT * FROM projects`;
+  connection.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else if (results.length === 0) {
+      res.status(404).json({ error: 'ressource not found or empty !' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 projectRouter.get('/:id', (req, res) => {
-    res.send('A one project')
+  const { id } = req.params;
+  const sql = 'SELECT * FROM projects WHERE id=?';
+  connection.query(sql, [id], (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err });
+    } else if (results.length === 0) {
+      res.status(404).json({ error: 'ressource not found or empty !' });
+    } else {
+      res.status(200).json(results);
+    }
+  });
 });
 
 
@@ -36,6 +56,6 @@ app.use('/api/projects', projectRouter);
 app.listen(port, (err) => {
     if(err){
         throw new Error('Something...');
-    }
+    } 
     console.log(`Server is listening on port ${port}.`)
 });
