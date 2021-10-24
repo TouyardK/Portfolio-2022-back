e45dd2696e56a6a603602d0cdd43afb0ba6b2002
TouyardK <touyard.kevin@outlook.fr>
2021-10-24 10:40:40 +0200

    Add data sql

--- data/projects.sql
+++ data/projects.sql
@@ -12,10 +12,17 @@ USE workflow_projects;
 # Table: projects
 #------------------------------------------------------------
 
-CREATE TABLE projects(
+CREATE TABLE projects (
         id            Int  Auto_increment  NOT NULL ,
         title         Varchar (150) NOT NULL ,
         content Text NOT NULL ,
-        icon      Varchar (50) NULL ,
-)ENGINE=InnoDB;
+        icon      Varchar (50) NULL 
+) ENGINE=InnoDB;
 
+CREATE TABLE projects
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150),
+    content TEXT NOT NULL,
+    icon VARCHAR(50) NULL
+)
